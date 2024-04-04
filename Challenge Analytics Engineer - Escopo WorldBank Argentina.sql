1c89023073171115b4caaea20f97a103bf4c500b
Thais <165857585+thaisasantos@users.noreply.github.com>
2024-04-04 01:07:32 -0300

    Add files via upload

---
+++ Challenge Analytics Engineer - Escopo WorldBank Argentina.sql
@@ -0,0 +1,18 @@
+-- KPI Evolução de Assinaturas & Internet
+------------------------------------------------------------------------------------------------------------------
+-- Link Tableau Public: https://public.tableau.com/app/profile/thais.alonso/viz/WorldBankDevelopmentIndicators_17121960545090/DadosGerais
+
+-- Fonte de dados: https://datacatalog.worldbank.org/search/dataset/0037712/World-Development-Indicators
+
+-- Fonte de pesquisa utilizadas:
+-- Argentina congela preços de internet, tv e telefonia: https://veja.abril.com.br/mundo/argentina-congela-precos-de-internet-tv-e-telefonia
+-- Covid-19: como a Argentina se tornou um dos cinco países com mais casos no mundo: https://www.bbc.com/portuguese/internacional-54576548
+-- Claro conecta El Chaltén, na Argentina, com internet móvel: https://www.bnamericas.com/pt/noticias/claro-conecta-el-chalten-na-argentina-com-internet-movel
+-- Crise na Argentina, corrida por internet via satélite e o que importa no mercado: https://www1.folha.uol.com.br/mercado/2022/07/crise-na-argentina-corrida-por-internet-via-satelite-e-o-que-importa-no-mercado.shtml
+
+-- Conclusão / Insights
+-- Dados mais recentes (2022) relacionados à argentina conta com uma população total de 46M, sendo 92% espaço urbano ocupado.
+-- É fato que comparado aos países de grande população, como china, índia, estados unidos e afins, vimos que a argentina não se destaca nos indicadores de crescente número de assinaturas de internet fixa, celular e telefone.
+-- Mas de maneira individual, temos um pico na evolução de usuários que utilizam a internet a partir de 2019 com um aumento de 6% à 8% comparado aos anos seguintes. O que nos trás a memória o ano corte de início da pandemia de COVID-19 e a necessidade do isolamento obrigatório, onde o universo virtual é um dos poucos meios de comunicação para compra de alimentos, transações bancárias ou até mesmo ter o conhecimento sobre seus parentes e amigos queridos. Ou seja, nos tornamos cada vez mais dependentes da internet. Lembrando que a argentina foi classificada como quinto país com maior registro de casos de COVID.
+-- O governo argentino precisou promover a tecnologia e inclusão digital. Realizando investimento na cobertura móvel, preços mais acessíveis e na infraestrutura de áreas rurais. O que colaborou para o alcance de 88% da população com acesso à internet.
+-- Ainda assim, é importante destacar que temos fatores de risco não calculados como a inflação, desigualdade socialeconômica (40% população abaixo da pobreza) e a segurança nos dados que estão sendo armazenados.
